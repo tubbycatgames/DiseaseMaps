b208d43ee4961daa739632dacb8e497d829a24e7
Phil Simmons <phil@tubbycatgames.com>
2020-04-26 01:04:48 -0400

    Added Dynamic Start Position Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,40 @@
-import React from "react"
+import React, { Component } from "react"
 import { Map, TileLayer } from "react-leaflet"
 
 import SEO from "../components/seo"
 
 import styles from "./index.module.css"
 
-const position = [39.9526, -75.1652]
-const IndexPage = () =>
-  typeof window !== "undefined" ? (
-    <Map className={styles.map} center={position} zoom={13}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <SEO title="Home" />
-    </Map>
-  ) : null
+const startZoom = 6
 
-export default IndexPage
+export default class IndexPage extends Component {
+  state = {
+    startPosition: undefined,
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const startPosition = [coords.latitude, coords.longitude]
+        this.setState({ startPosition })
+      },
+      () => this.setState({ startPosition: [0, 0] })
+    )
+  }
+
+  render() {
+    return typeof window !== "undefined" && this.state.startPosition ? (
+      <Map
+        className={styles.map}
+        center={this.state.startPosition}
+        zoom={startZoom}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <SEO title="Home" />
+      </Map>
+    ) : null
+  }
+}
